d392694be20a862bd3f3651e37af95aba34ac812
Abhishek Thakur <thakura204@gmail.com>
2019-11-18 21:47:58 +0530

    [#5] [Abhishek] | Make order id auto generated

--- src/main/java/order/requests/CreateOrderRequest.java
+++ src/main/java/order/requests/CreateOrderRequest.java
@@ -11,7 +11,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateOrderRequest {
-    private String id;
     private String description;
     private List<Item> items;
 }
