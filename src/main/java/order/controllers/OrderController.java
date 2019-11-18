5689b53a44cb8c036ad6fad55cda65a4d1304e33
Abhishek Thakur <thakura204@gmail.com>
2019-11-18 20:50:59 +0530

    [#4] Abhishek | Add repository layer which insert data in dynamo db also added serverless.yml to deploy lambda and resources

--- src/main/java/order/controllers/OrderController.java
+++ src/main/java/order/controllers/OrderController.java
@@ -5,22 +5,29 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import order.models.Order;
+import order.repository.Repository;
 import order.requests.CreateOrderRequest;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+
 @Controller("/v1")
 public class OrderController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+
+    @Inject
+    private Repository repository;
 
     @Post("/order")
     public HttpResponse createOrder(@Body CreateOrderRequest orderRequest) {
         LOGGER.info("create order request received: {}", orderRequest);
         ModelMapper modelMapper = getModelMapper();
         Order order = modelMapper.map(orderRequest, Order.class);
+        repository.save(order);
         return HttpResponse.created(order);
     }
 
--- src/main/java/order/models/Item.java
+++ src/main/java/order/models/Item.java
@@ -1,5 +1,7 @@
 package order.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
+@DynamoDBDocument
 public class Item {
 
+    @DynamoDBAttribute
     private String id;
+
+    @DynamoDBAttribute
     private String description;
+
+    @DynamoDBAttribute
     private String price;
 }
